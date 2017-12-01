4db612417aeaec4608cacaa2ce99459693aa2f0b
airyballz <robert.brenner1@icloud.com>
2017-12-01 05:12:43 -0600

    First commit

---
+++ database_setup.py
@@ -0,0 +1,114 @@
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime, Text, LargeBinary
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+class Category(Base):
+
+    __tablename__ = "categories"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255), unique=True, nullable = False)
+    items = relationship("Item", backref="category")
+
+    @property
+    def serialize(self):
+        return {
+            'id' : self.id,
+            'name' : self.name,
+            'items' : [item.serialize for item in self.items]
+        }
+
+    def serializeToXml(self, content):
+        content.append("<Category>")
+        content.append("<ID>%s</ID>" % self.id)
+        content.append("<Name>%s</Name>" % self.name)
+
+        if self.items:
+            content.append("<Items>")
+
+            for item in self.items:
+                item.serializeToXml(content)
+
+            content.append("</Items>")
+        else:
+            content.append("<Items/>")
+
+        content.append("</Category>")
+
+class Item(Base):
+
+    __tablename__ = "items"
+
+    id = Column(Integer, primary_key = True)
+    name = Column(String(255), nullable = False)
+    description = Column(Text, nullable = True)
+    creation_date = Column(DateTime, nullable = False)
+    picture = Column(Text, nullable = True)
+    picture_data = Column(LargeBinary, nullable = True)
+
+    category_id = Column(Integer, ForeignKey("categories.id"))
+
+    @property
+    def serialize(self):
+        return {
+            'id' : self.id,
+            'name' : self.name,
+            'description' : self.description,
+            'creation_date' : self.creation_date
+        }
+
+    def serializeToXml(self, content):
+        content.append("<Item>")
+        content.append("<ID>%s</ID>" % self.id)
+        content.append("<Name>%s</Name>" % self.name)
+
+        if self.description:
+            content.append("<Description>%s</Description>" % self.description)
+        else:
+            content.append("<Description/>")
+
+        content.append("<CreationDate>%s</CreationDate>" % self.creation_date.isoformat())
+
+        content.append("</Item>")
+
+class User(Base):
+
+    __tablename__ = "users"
+    
+     # the id from Google is to big to be stored in an Integer
+    id = Column(String(255), primary_key = True)
+    name = Column(String(255), nullable = False)
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return self.id
+
+if __name__ == '__main__':
+    engine = create_engine('sqlite:///itemcatalog.db')
+    Base.metadata.create_all(engine)
+
+    DBSession = sessionmaker(bind=engine)
+    session = DBSession()
+
+    session.add(Category(name="Action"))
+    session.add(Category(name="Adventure"))
+    session.add(Category(name="Comedy"))
+    session.add(Category(name="Crime & Gangster"))
+    session.add(Category(name="Drama"))
+    session.add(Category(name="Horror"))
+    session.add(Category(name="Musicals"))
+    session.add(Category(name="Science Fiction"))
+    session.add(Category(name="War"))
+
+    session.commit()
